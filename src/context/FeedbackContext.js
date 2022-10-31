4c3e9b40d412b80c2938bc2c1ef7399d0e1e00f4
vrundanawal <nawal.vrunda@gmail.com>
2022-10-31 10:47:24 +0000

    create a context provider

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+const feedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      rating: 10,
+      text: "This is feedback item 1 from Context ",
+    },
+  ]);
+  return (
+    <feedbackContext.Provider value={{ feedback: feedback }}>
+      {children}
+    </feedbackContext.Provider>
+  );
+};
+
+export default feedbackContext;
